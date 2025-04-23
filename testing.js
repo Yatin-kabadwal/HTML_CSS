d936ff29866529dd73fadd67956f5bbb3d92b44b
Yatin kabadwal <yatinkabdwal@gmail.com>
2025-04-23 16:57:55 +0530

    Commit

--- testing.js
+++ testing.js
@@ -220,29 +220,304 @@
 // }
 
 
-let fullname = "Yatin kabadwal";
-for(let val of fullname){
-    console.log(val);
-}
+// let fullname = "Yatin kabadwal";
+// for(let val of fullname){
+//     console.log(val);
+// }
 
-const arr = [10,20,30,40,50];
-function getsum(arr){
-    let length = arr.length;
-    let sum =0;
+// const arr = [10,20,30,40,50];
+// function getsum(arr){
+//     let length = arr.length;
+//     let sum =0;
 
-    //Done by tradational for loop method
-    for (i=0; i<length; i++){
-        sum += arr[i];
-    }
+//     //Done by tradational for loop method
+//     for (i=0; i<length; i++){
+//         sum += arr[i];
+//     }
 
-    //done by for each loop
-    let num = 0 ;
-    arr.forEach(element => {
-        num = num + element
-    });
-    console.log("Your total sum is: ",sum)
-    console.log("Your total num is: ",num)
+//     //done by for each loop
+//     let num = 0 ;
+//     arr.forEach(element => {
+//         num = num + element
+//     });
+//     console.log("Your total sum is: ",sum)
+//     console.log("Your total num is: ",num)
 
-}
+// }
 
-getsum(arr);
+// getsum(arr);
+
+
+
+//New topic  variable hoisting;
+
+
+// Printname();
+
+// function Printname(){
+//     console.log("Yatin kabadwal");
+// }
+
+// printname();
+// let printname = function(){
+//     console.log("Hi how are you all");
+// }
+
+// console.log(nam);
+
+// var nam = "Yatin";
+
+// Always remember that the hoisting can only be performed on the normal function it will not work in the expression one or the arrow one ();
+
+
+
+// class abc {
+//     d() {
+//         console.log("In function d");
+//     }c() {
+//         console.log("In function c");
+//        this. d();
+//     }b() {
+//         console.log("In function b");
+//        this. c();
+//     }a() {
+//         console.log("In function a");
+//        this. b();
+//     }
+// }
+
+
+// const ex = new abc();
+// ex.a();
+
+
+// function greetMe(greet, fullname){
+//     console.log("Hello ",fullname);
+//     greet();
+// }
+
+// function greet() {
+//     console.log("Greetings of the day");
+// }
+
+// greetMe(greet,"Yatin");
+
+
+
+
+
+//Function inside a function
+// function outer() {
+//     console.log("Hello from outer function");
+//     return function inner() {
+//       console.log("Hello from inner function!");
+//     }
+//   }
+//   const result = outer(); // result is now the inner function
+// result(); 
+
+
+
+
+//Functions inside the array;
+
+// const arr = [
+//     function a(a,b){
+//         return a+b;
+//     },
+//     function b(a,b){
+//         return a-b;
+//     },
+//     function c(a,b){
+//         return a*b;
+//     }
+// ];
+
+// let first = arr[0];
+// let ans = first(10,23);
+// console.log(ans);
+
+
+// let num ;
+// for (let i = 0; i < arr.length; i++) {
+//     num = arr[i];
+//     console.log("The function is:", num);
+//     console.log("Result of calling function with 5, 3:", num(5, 3));
+// }
+
+
+
+//VAriable scoping : 
+
+
+//global scope
+
+// var age = 12; 
+
+// console.log(age)
+
+
+// {
+//     console.log(age)
+// }
+
+
+// if(true)
+//     console.log(age);
+
+// for( let i=0; i<=2; i++){
+//     console.log(age , "This is from for loop");
+// }
+
+
+// function sayHello(){
+//     console.log("Hi",age);
+// }
+
+// sayHello();
+
+
+//Function scope
+
+// function sayHello(){
+//     let name = "Hello world";
+//     console.log(name);
+// }
+
+// sayHello();
+
+
+// Block scope ; Always remember that var is not the block scope :
+
+// {
+//     var na = "Yatin";
+// }
+// console.log(na);
+
+
+// If we don't mark any variable like private of other os by default it will be public .
+// class a {
+    
+   
+//     gender = "xyz"
+// }
+
+// class b {
+//    #age = 25;  //Private;
+    
+//    ag(){
+//     console.log(this.#age);
+//    }
+
+//    get fetchange(){
+//     return this.#age;
+//    }
+
+//    set modifyage(a){
+//     this.#age = a;
+//    }
+// }
+
+// let ab = new b();
+
+// console.log(ab.age);
+// ab.ag();
+
+
+// let g = ab.fetchange;
+
+// console.log(g);
+
+// ab.modifyage(40);
+// console.log(ab.fetchange);
+
+
+
+// class Human{
+//     weight;
+//     #age = 34;
+//     ht = 332;
+
+//     constructor (age, weight){
+//         this.#age = age;
+//         this.weight = weight;
+//     }
+    
+//     abc(){
+//         console.log(this.#age);
+//     }
+// }
+
+// let obj = new Human(55,23);
+// console.log(obj.ht);
+// console.log(obj.weight);
+// console.log(obj.age);
+// obj.abc();
+
+
+//Default parameters
+
+// function xyz (h="Default value") {
+//     console.log(h)
+// }
+// xyz();
+// xyz("Yatin kabadwal");
+// xyz(null);
+// xyz(undefined);
+// xyz(23423);
+
+
+// console.log
+
+
+// Using function in default parameter;
+// function getage(){
+//     return 23;
+// }
+
+// function utility(name = "Rana",age=getage()){
+//     console.log(name, " ", age);
+// }
+
+// utility();
+// utility("xyz",44);
+// utility("Ji")
+
+
+
+
+// console.log(Math.PI);
+// console.log(Math.sin);
+
+// let arr = [ 2,3,4,5,6,7,8,0];
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+// console.log(Math.round(...arr));
+// console.log(Math.E);
+
+
+// let current = new Date();
+// console.log(current);
+// let date = new Date('june 23 1993 32:32');
+// console.log(date);
+
+
+//Objects are dynamic in nature 
+let obj = {
+    age : 23,
+    wt : 34,
+    ht : 232
+};
+// obj.age = 34;
+// console.log(obj);
+// obj.age= 23;
+// obj.color = "yellow";
+// console.log(obj);
+
+let clone = {...obj};
+obj.age = 19;
+console.log("clone: ",clone);
+console.log("obj: ",obj);
+ //Another type of cloning is there known as destination cloning;
+ let dest = Object.assign({},obj);
+ console.log(dest);
