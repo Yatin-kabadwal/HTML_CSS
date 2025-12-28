58956fbc5f57556fec5a964057ccbe4c56c79902
Yatin kabadwal <yatinkabdwal@gmail.com>
2025-12-28 13:19:03 +0530

    COmmit

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -7,6 +7,12 @@ if (true){
     console.log(age)
 }
 
-
+let consa = 23
+if (consa === 23 & age ==="25"){
+    console.log("TRUE")
+}
+else{
+    console.log("FALSE")
+}
 let marks ;
 console.log(marks);
